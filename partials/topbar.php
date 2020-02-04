f9e1fab4c453552eb120b73ed6aa2f746a32e1f5
Olivier van Duuren <olivier96@live.nl>
2020-02-04 20:52:00 +0100

    Update topbar /img

--- partials/topbar.php
+++ partials/topbar.php
@@ -19,7 +19,7 @@
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-gray-600 small">
           <!--ingelogd als: <?php echo $_SESSION['role'] . "  " ?>--><b><?php echo $_SESSION['surname'] . " " . $_SESSION['lastname'] ?></b></span>
-        <img class="img-profile rounded-circle" src="img\<?php echo $_SESSION['role'] ?>_Icon.png">
+        <img class="img-profile rounded-circle" src="img/<?php echo $_SESSION['role'] ?>_Icon.png">
       </a>
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
