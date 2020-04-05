df7e4ad43ce9708b3c54f67711b271c2064724a5
Floris DeJongh <fdejongh28@gmail.com>
2020-04-05 13:57:36 +0200

    .gitignore is now working

--- php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","fmpmusicproject");
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
